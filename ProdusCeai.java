85936f3fc78ed69397d6a1eab14422f2ae53946d
Anda77 <andadeacu@yahoo.com>
2017-11-27 22:34:27 +0200

    ContentSpeed Homework

---
+++ ProdusCeai.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contentspeed;
+
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.interactions.Actions;
+
+public class ProdusCeai {
+
+    WebDriver driver;
+
+    By produsArmonia = By.xpath("//*[@id=\"product_addToCart\"]");
+    By finalizeazaComanda = By.xpath("//*[@id='popup-roo']/div[2]/div/a[3]");
+    By cosComanda = By.xpath("//*[@id=\"header\"]/div[2]/div/div[1]/div[4]/a/span");
+
+    public ProdusCeai(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickAdaugaCos() {
+
+        driver.findElement(produsArmonia).click();
+
+    }
+
+    public void clickFinalizeaza() {
+        try {
+
+            String MainWindow = driver.getWindowHandle();
+
+            // To handle all new opened window.				
+            Set<String> s1 = driver.getWindowHandles();
+            Iterator<String> i1 = s1.iterator();
+
+            while (i1.hasNext()) {
+                String ChildWindow = i1.next();
+
+                driver.switchTo().window(ChildWindow);
+
+                driver.findElement(By.partialLinkText("Finalizeaza comanda")).click();
+
+            }
+        } catch (WebDriverException ex) {
+            ex.printStackTrace();
+            //driver.quit();
+        }
+
+    }
+
+    void CosClick() {
+        //driver.findElement(cosComanda).click();
+
+        Actions actions = new Actions(driver);
+        actions.click(driver.findElement(cosComanda)).perform();
+        String MainWindow = driver.getWindowHandle();
+
+        Set<String> s1 = driver.getWindowHandles();
+        Iterator<String> i1 = s1.iterator();
+
+       
+
+        while (i1.hasNext()) {
+            String ChildWindow = i1.next();
+
+          
+            driver.switchTo().window(ChildWindow);
+
+            driver.findElement(By.partialLinkText("Vezi cosul")).click();
+
+        }
+    }
+
+    void Actions() {
+        clickAdaugaCos();
+        clickFinalizeaza();
+        CosClick();
+    }
+
+}
