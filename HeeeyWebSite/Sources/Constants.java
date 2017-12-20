07b9c8bcc9f98493b97acc1027972324f59afd5a
Anda77 <andadeacu@yahoo.com>
2017-12-20 22:44:50 +0200

    adaug un produs in cos 4 browsere

---
+++ HeeeyWebSite/Sources/Constants.java
@@ -0,0 +1,14 @@
+package contentspeed;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class Constants {
+
+    public static final String URLProdus = "https://www.heeey.ro/decoratiuni/vaze-si-boluri/bol-decorativ-aluminiu-auriu-dimensiuni-25-x-25-x-12-cm-h388.html";
+
+    public static final String URLRoot = "https://www.heeey.ro/";
+
+}
